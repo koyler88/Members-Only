238bcac41191fd710cac5e1380cd7131751faad4
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2025-06-23 13:41:45 -0600

    Integrate Railway Connection

--- db/pool.js
+++ db/pool.js
@@ -3,4 +3,5 @@ require("dotenv").config();
 
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
